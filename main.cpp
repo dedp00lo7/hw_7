f1af755cdf7bb6cf95bd6ef4cd739eb62e08a8fe
dedp00lo7 <93996732+dedp00lo7@users.noreply.github.com>
2022-02-15 20:36:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,82 @@
-
-
 #include <iostream>
+
 using namespace std;
-
-
-void qs(int *items, int left, int right)
+int swapInt(int a, int b)
 {
-    int i, j;
-  char x, y;
-
-  i = left; j = right;
-  x = items[(left+right)/2]; /* выбор компаранда */
-
-  do {
-    while((items[i] < x) && (i < right)) i++;
-    while((x < items[j]) && (j > left)) j--;
-
-    if(i <= j) {
-      y = items[i];
-      items[i] = items[j];
-      items[j] = y;
-      i++; j--;
+    int temp;
+    temp = a;
+        a = b;
+        b = temp;
+    return static_cast<void>(a),b;
+}
+void sortInserts(int *array, int first, int last) {
+    int temp, pos, i;
+    for (i = first + 1; i <= last; i++){
+        temp = array[i];
+        pos = i-1;
+        while (pos >= first && array[pos] > temp) {
+            array[pos + 1] = array[pos];
+            pos = pos -1 ;
+            
     }
-  } while(i <= j);
+    
+        array[pos + 1] = temp;
+   }
+}
 
-  if(left < j) qs(items, left, j);
-  if(i < right) qs(items, i, right);
+int median ( int *arr, int first, int last){
+    int result;
+    int min, max;
+    int middle = arr[(first + last) / 2];
+    if(arr[first] > arr[middle])
+    {
+        min = arr[middle];
+        max = arr[first];
+    } else{
+        min = arr[first];
+        max = arr[middle];
+    }
+    if (min > arr[last]) result = min;
+    else if (arr[last] > max) result = max;
+    else result = arr[last];
+    
+    if (result == arr[first]) return first;
+    else if (result == arr[middle]) return middle;
+    else return last;
+}
+void quicksort(int* arr, int first, int last)
+{
+    
+    if (last - first  > 10){
+        int med = median(arr, first, last);
+        swap(arr[med],  arr[ (first + last) / 2]);
+        int i = first;
+        int j = last;
+        int x = arr[ (first + last) / 2];
+        do {
+            while (arr[i] < x) i++;
+            while (arr [i] > x) j--;
+            if( i <= j) {
+                swap(arr[i], arr[j]);
+                i++;
+                j--;
+            }
+        }while (i <= j);
+        
+        quicksort(arr , i, last);
+        quicksort(arr , first, j);
+        
+    }else
+    {
+        sortInserts ( arr, first, last);
+        return;
+    }
+        
+    
 }
 int main()
-{
-    const int SIZE = 40;
-    int arr[SIZE] = {  };
-    qs(arr, 0 , SIZE-1);
+    {
     
-   
-     
-       
     return 0;
 }
+
