a2b6aafdb1b1e7d932cdee2363bae3665fda1f4c
dedp00lo7 <93996732+dedp00lo7@users.noreply.github.com>
2022-02-15 20:59:15 +0200

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -3,29 +3,39 @@
 #include <iostream>
  
     using namespace std;
- 
-int main() {
-    int n;
-    cout << "Enter an array size:\n";
-    cout << "n = ";
-    cin >> n;
-    int* arr = new int[n];
-    cout << "Enter an array:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+ void bucketort(int * arr,const int len)
+{
+    const int b = 10;
+    int buckets[b][len+1];
+    for (int i = 0; i < b; ++i) {
+        buckets[i][len] = 0;
     }
-    for (int i = 0; i < n; i++) {
-        for (int j = n - 1; j > i; j--) {
-            if (arr[i] % 2 == 0 && arr[j] % 2 == 0 && arr[i] > arr[j]) {
-                swap(arr[i], arr[j]);
+    for ( int digit  =1; digit < 1000000000; digit *= 10)
+    {
+        for ( int i = 0; i < len; ++i)
+        {
+            if (arr[i]%2 == 0)
+            {
+                int d = (arr[i] / digit) % b;
+                buckets[d][buckets[d][len]++] = arr[i];
+            arr[i] = -1;
             }
         }
+        int idx = 0;
+        for ( int i = 0; i < b; ++i)
+        {
+            for (int j = 0; j < buckets[i][len]; ++j)
+                 {
+                    while (arr[idx] != -1) idx++;
+                                                    
+                    arr[idx] = buckets[i][j];
+                }
+            buckets[i][len]= 0;
+        }
     }
-    cout << "Output of the program:\n";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    delete [] arr;
-
+}
+int main()
+    {
+    
     return 0;
 }
